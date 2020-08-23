5ff16a815779a742a0b3f3ec067436300d727c1a
Tic <44981148+yougotsyskeyed@users.noreply.github.com>
2020-08-23 09:39:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-import discord, os, keep_alive, asyncio, datetime, pytz
-
+import discord
 
 from discord.ext import tasks, commands
 
 client = commands.Bot(
-  command_prefix=':',
-  self_bot=True
+    command_prefix=':',
+    self_bot=True
 )
 
 
-
 @client.event
-async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = "yes", url = "https://www.twitch.tv/yougotsyskeyed"))
+async def on_connect() :
+    await client.change_presence(activity=discord.Streaming(name="yes", url="https://www.twitch.tv/yougotsyskeyed"))
 
 
-keep_alive.keep_alive()
-client.run('your account token', bot=False)
+client.run('account token', bot=False)
